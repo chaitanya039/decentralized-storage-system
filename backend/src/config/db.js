8191d7653124d1d8f22d8f9599b8c13a277f76d4
Chaitanya Pansare <chaitanyapansare33607@gmail.com>
2026-02-01 22:37:15 +0530

    fix: load environment variables correctly before DB initialization

---
+++ backend/src/config/db.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+import pkg from "pg";
+const { Pool } = pkg;
+
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+pool.on("connect", () => {
+  console.log("✅ PostgreSQL connected");
+});
+
+pool.on("error", (err) => {
+  console.error("❌ PostgreSQL pool error", err);
+  process.exit(1);
+});
+
+export default pool;
