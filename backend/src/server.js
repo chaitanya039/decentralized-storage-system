0b9cdc144aae2331015a078f104b1d29ef3f8fc9
Chaitanya Pansare <chaitanyapansare33607@gmail.com>
2026-02-16 16:19:26 +0530

    Resetup of Database Connection using Sequelize Framework.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 
 import app from "./app.js";
-import pool from "./config/db.js";
-
-// Database connection
-(
-    async () => {
-        try {
-            const result = await pool.query("SELECT version()");
-            console.log(result.rows[0]);
-        } catch(error) {
-            console.log("DB Connection Error: ", error.message);
-        }
-    }
-)();
+import { connectDB } from "./config/dbInit.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`);
-});
+  });
+};
+
+startServer();
